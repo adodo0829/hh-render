dc00cf909a9dceed49c001e1171600b5637c00ed
huhua <huhua@megvii.com>
2024-12-04 09:45:05 +0800

    feat: add lc1

---
+++ src/utils/leetcode100/1-10.js
@@ -0,0 +1,23 @@
+/**
+ * 编写一个程序，找到两个单链表相交的起始节点。
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) {
+    return null;
+  }
+
+  let hashMap = new Map();
+  let tempA = headA;
+  while (tempA) {
+    hashMap.set(tempA, 1);
+    tempA = tempA.next;
+  }
+
+  let tempB = headB;
+  while (tempB) {
+    if (hashMap.has(tempB)) {
+      return tempB;
+    }
+    tempB = tempB.next;
+  }
+};
