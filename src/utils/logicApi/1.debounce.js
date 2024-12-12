f8da166d7e7526e3870810684ccb0eb16b851145
huhua <huhua@megvii.com>
2024-12-12 09:44:36 +0800

    feat: optimzie

--- src/utils/logicApi/1.debounce.js
+++ src/utils/logicApi/1.debounce.js
@@ -1,3 +1,18 @@
+// 一直一直点触发，n秒后才真正执行
+function debounce1(callback, delay) {
+  let timeID = null;
+  return function (...args) {
+    if (timeID) {
+      // 取消timeID绑定的延迟执行的函数
+      window.clearTimeout(timeID);
+    }
+    timeID = setTimeout(() => {
+      callback.apply(this, args);
+      timeID = null;
+    }, delay);
+  };
+}
+
 export function debounce(func, delay, rightNow = false) {
   // - 延迟执行
   // - 延迟期间再被触发，重新计时
--- src/utils/logicApi/2.throttle.js
+++ src/utils/logicApi/2.throttle.js
@@ -1,13 +1,37 @@
-export function throttle(func, delay) {
-  // 事件触发期间，n 秒内只执行一次
-  let flag = true;
+// 一直一直触发，按固定频率执行: 用开关来控制
+
+function throttle1(callback, limitTime) {
+  let timeID = null;
   return function (...args) {
-    if (flag) {
-      func.apply(this, args);
-      flag = false;
-      setTimeout(() => {
-        flag = true;
-      }, delay);
+    // 非立即执行版， 最后一次执行
+    if (timeID) {
+      return;
+    }
+    timeID = setTimeout(() => {
+      callback.apply(this, args);
+      timeID = null;
+    }, limitTime);
+  };
+}
+
+// 立即执行版， 首次执行，最后一次也调用
+function throttle2(fn, limitTime) {
+  let timeID = null;
+  let tempTime = 0;
+
+  return function (...args) {
+    // 首次系统时间一般都比limit大，会立即执行
+    // 后面就是增量时间了
+    const now = Date.now();
+    clearTimeout(timeID);
+    if (now - tempTime >= limitTime) {
+      fn.apply(this, args);
+      tempTime = Date.now();
+    } else {
+      // 为了最后一次触发可以调用函数
+      timeID = setTimeout(() => {
+        fn.apply(this, args);
+      }, limitTime);
     }
   };
 }
